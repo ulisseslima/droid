b2424b353f67a1a9d01813c0771d08b3978e461a
dvlcube <ulisses@dvlcube.com>
2013-06-27 01:09:48 -0300

    cube file

---
+++ src/main/java/com/dvlcube/util/CubeFile.java
@@ -0,0 +1,41 @@
+package com.dvlcube.util;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * 
+ * @since 25/06/2013
+ * @author wonka
+ */
+public class CubeFile {
+	public File file;
+
+	public CubeFile(String path) {
+		this(path, true);
+	}
+
+	/**
+	 * @param path
+	 * @since 25/06/2013
+	 * @author wonka
+	 */
+	public CubeFile(String path, boolean create) {
+		file = new File(path);
+		if (!file.isFile()) {
+			if (create) {
+				try {
+					file.createNewFile();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			} else {
+				throw new IllegalArgumentException("'" + path + "' is not a file");
+			}
+		}
+	}
+
+	public boolean isDir() {
+		return file.isDirectory();
+	}
+}
