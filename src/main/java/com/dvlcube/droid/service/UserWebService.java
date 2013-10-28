635faf9abe9baab553606c127dc0a5e30a51dd18
dvlcube <ulisses@dvlcube.com>
2013-10-27 22:06:35 -0200

    no duplicates

--- src/main/java/com/dvlcube/droid/service/UserWebService.java
+++ src/main/java/com/dvlcube/droid/service/UserWebService.java
@@ -1,13 +1,23 @@
 package com.dvlcube.droid.service;
 
+import java.util.List;
+
+import org.hibernate.criterion.Restrictions;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
+import com.dvlcube.bean.Child;
+import com.dvlcube.cuber.CubeGenerics;
 import com.dvlcube.droid.bean.Role;
 import com.dvlcube.droid.bean.User;
+import com.dvlcube.droid.dao.EventDao;
 import com.dvlcube.droid.dao.RoleDao;
 import com.dvlcube.droid.dao.UserDao;
 import com.dvlcube.droid.service.rr.NewUserRequest;
+import com.dvlcube.droid.service.rr.UpdateLocationRequest;
+import com.dvlcube.service.BasicInfo;
 import com.dvlcube.service.Response;
 import com.dvlcube.service.ServiceTemplate;
 
@@ -22,11 +32,36 @@ public class UserWebService extends ServiceTemplate<User> implements UserService
 	private UserDao dao;
 
 	@Autowired
+	private EventDao eventDao;
+
+	@Autowired
 	private RoleDao roleDao;
 
+	@Override
+	public Response<User> add(User entity) {
+		return super.add(entity);
+	}
+
+	@Override
+	public Response<User> addUser(NewUserRequest userRequest) {
+		User existingUser = getByUsername(userRequest.getUsername());
+		if (existingUser == null) {
+			User user = new User(userRequest);
+			Response<User> response = add(user);
+			roleDao.create(new Role(response.getContent()));
+			return response;
+		} else {
+			return new Response<>(false);
+		}
+	}
+
 	public User getByUsername(String username) {
-		User user = dao.retrieve(getT(), new User(username));
-		return user;
+		List<User> contents = list(Restrictions.eq("name", username)).getContents();
+		if (contents != null && contents.size() > 0) {
+			User user = contents.get(0);
+			return user;
+		}
+		return null;
 	}
 
 	@Override
@@ -35,15 +70,47 @@ public class UserWebService extends ServiceTemplate<User> implements UserService
 	}
 
 	@Override
+	public User getSession() {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		return getByUsername(auth.getName());
+	}
+
+	@Override
 	protected Class<User> getT() {
 		return User.class;
 	}
 
 	@Override
-	public Response<User> newUser(NewUserRequest userRequest) {
-		User user = new User(userRequest);
-		Response<User> response = add(user);
-		roleDao.create(new Role(response.getContent()));
-		return response;
+	public Response<User> listUsersOnSamePage(UpdateLocationRequest request) {
+		BasicInfo subject = load(request.getElement());
+		if (subject != null && subject instanceof Child) {
+			List<String> listSiblings = listSiblings(CubeGenerics.<Child<? extends BasicInfo>> unchecked(subject));
+			return list(//
+					Restrictions.in("element", listSiblings),//
+					Restrictions.eq("active", true),//
+					Restrictions.not(//
+							Restrictions.eq("id", request.getUserId())));
+		}
+		return new Response<>(false);
+	}
+
+	@Override
+	public void setOnlineStatus(String userName, boolean isOnline) {
+		if (userName != null) {
+			User user = getByUsername(userName);
+			user.setOnline(isOnline);
+			if (!isOnline) {
+				user.setActive(false);
+			}
+			update(user).getContent();
+		}
+	}
+
+	@Override
+	public User updateLocation(UpdateLocationRequest request) {
+		User user = new User(request.getUserId());
+		user.setElement(request.getElement());
+		user.setActive(request.isActive());
+		return update(user).getContent();
 	}
 }
